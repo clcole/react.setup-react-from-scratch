6cb75dfd4f852d03bed1ce801b4a058c2ddeeb2e
CLCole <clcole@gmail.com>
2019-10-18 20:40:29 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>
+          Edit <code>src/App.js</code> and save to reload
+        </h2>
+    </div>
+  );
+}
+export default App;
