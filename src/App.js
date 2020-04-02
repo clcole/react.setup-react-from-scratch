3f4c5d3f32f66d0ddd364f68837544016eecbac0
CLCole <clcole@gmail.com>
2020-04-02 17:41:10 -0500

    test change to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
         <h2>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </h2>
     </div>
   );
